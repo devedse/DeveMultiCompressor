4fcce955dfb8bb867d0556277baa36cf415c650d
Devedse <davydavidse@gmail.com>
2016-08-31 09:34:55 +0200

    Added one more unit test

--- DeveMultiCompressorTests/FullFlowTests.cs
+++ DeveMultiCompressorTests/FullFlowTests.cs
@@ -50,5 +50,47 @@ namespace DeveMultiCompressorTests
                 Assert.IsNotNull(lzma.GenerateHash());
             }
         }
+
+        [TestMethod]
+        public void CompressSimpleFileWithPrecomp()
+        {
+            Constants.CompressorFolder = "TestCompressors";
+
+            var inputFile = Path.Combine(FolderHelperMethods.AssemblyDirectory.Value, "TestFiles", "enwik6.txt");
+            var outputFileLzma2 = Path.Combine(FolderHelperMethods.AssemblyDirectory.Value, "Output", "enwik6_LZMA2.7z");
+            var outputFileLzma = Path.Combine(FolderHelperMethods.AssemblyDirectory.Value, "Output", "enwik6_LZMA.7z");
+
+            using (var container = DryContainer.CreateDryContainer())
+            {
+                var runner = container.Resolve<CompressorRunner>();
+
+                if (File.Exists(outputFileLzma2))
+                {
+                    File.Delete(outputFileLzma2);
+                }
+                if (File.Exists(outputFileLzma))
+                {
+                    File.Delete(outputFileLzma);
+                }
+
+                var commandLineOptions = new CommandLineOptions()
+                {
+                    InputFile = inputFile,
+                    UsePrecomp = true,
+                    Verify = true
+                };
+
+                runner.Go(commandLineOptions);
+
+                Assert.IsTrue(File.Exists(outputFileLzma2));
+                Assert.IsTrue(File.Exists(outputFileLzma));
+
+                var lzma2 = new CompressorFileInfo(outputFileLzma2);
+                var lzma = new CompressorFileInfo(outputFileLzma);
+
+                Assert.IsNotNull(lzma2.GenerateHash());
+                Assert.IsNotNull(lzma.GenerateHash());
+            }
+        }
     }
 }
