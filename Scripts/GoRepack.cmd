3c871a828848b7aeeb7e169de77c47a43595e4d5
Devedse <davydavidse@gmail.com>
2016-09-03 17:59:11 +0200

    More test1123

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -7,16 +7,16 @@ del "%~dp0DeveMultiCompressor.zip"
 cd "%~dp0Output"
 dir
 
-xcopy "%~dp0..\DeveMultiCompressor\bin\Release\Compressors" "Output\Compressors\" /e /y
-xcopy "%~dp0..\DeveMultiCompressor\bin\Release\Precompressors" "Output\Precompressors\" /e /y
+xcopy "%~dp0..\DeveMultiCompressor\bin\Release\Compressors" "%~dp0Output\Compressors\" /e /y
+xcopy "%~dp0..\DeveMultiCompressor\bin\Release\Precompressors" "%~dp0Output\Precompressors\" /e /y
 
-xcopy "%~dp0\SampleScripts" "Output\" /e /y
+xcopy "%~dp0SampleScripts" "%~dp0Output\" /e /y
 
 cd "%~dp0Output"
 dir
 
-"%~dp0\7z_x64_1602\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveMultiCompressor.zip" ".\Output\*"
-"%~dp0\7z_x64_1602\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveMultiCompressor.7z" ".\Output\*"
+"%~dp0\7z_x64_1602\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveMultiCompressor.zip" "%~dp0Output\*"
+"%~dp0\7z_x64_1602\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveMultiCompressor.7z" "%~dp0Output\*"
 
 cd "%~dp0Output"
 dir
