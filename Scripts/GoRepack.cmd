7ae01b95ba5346b8bfc8827761768de273497824
Devedse <davydavidse@gmail.com>
2016-09-03 17:56:54 +0200

    Another test

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -12,5 +12,11 @@ xcopy "%~dp0..\DeveMultiCompressor\bin\Release\Precompressors" "Output\Precompre
 
 xcopy "%~dp0\SampleScripts" "Output\" /e /y
 
+cd "%~dp0Output"
+dir
+
 "%~dp0\7z_x64_1602\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveMultiCompressor.zip" ".\Output\*"
 "%~dp0\7z_x64_1602\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveMultiCompressor.7z" ".\Output\*"
+
+cd "%~dp0Output"
+dir
